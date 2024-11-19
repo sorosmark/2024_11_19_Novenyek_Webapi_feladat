1564f868c1b1c925dc55a563774fdaf7fba772c1
sorosmark <soros.mark@diak.szbi-pg.hu>
2024-11-19 09:30:24 +0100

    updated py

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-
+from flask import Flask, render_template, request, redirect, url_for
+app = Flask(__name__)
 plants = [
     {"name": "Monstera", "type": "Szobanövény", "feature": "Nagyméretű levelek"},
     {"name": "Bambusz", "type": "Dísznövény", "feature": "Gyors növekedés"},
     {"name": "Kaktusz", "type": "Pozsgás", "feature": "Szárazságtűrő"}
 ]
 
+@app.route('/', methods=["GET"])
+def index():
+    return render_template('index.html')
+
+@app.route('/tabla', methods=["GET"])
+def tabla():
+    return render_template('tabla.html', plants=plants)
+
+if __name__ == "__main__":
+    app.run(debug=True)
